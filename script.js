c125f39dc3abf45c9afec34ed8d214dd77cf6874
Jhean Bryan <jheanbeyan123@gmail.com>
2022-08-14 19:18:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function verificaNumeroPrimo(numero){
-    if (numero == 1 || numero ==0){
-        console.log(`${numero} não é primo`)
-    }
-    let divisor = 2;
-    while(numero%divisor != 0)
-        divisor++;
+<html>
+    <script>
+        function verificaNumeroPrimo(numero){
+        if (numero == 1 || numero ==0){
+            console.log(`${numero} não é primo`)
+        }
+        let divisor = 2;
+        while(numero%divisor != 0)
+            divisor++;
 
-    if(numero == divisor)
-        console.log("Número é Primo!");
-    else
-        console.log("Número NÃO primo!");
-}
+        if(numero == divisor)
+            console.log("Número é Primo!");
+        else
+            console.log("Número NÃO primo!");
+    }
+    </script>
+<\html>
+
